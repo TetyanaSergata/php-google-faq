a5b8e96218652ec47e40c8fd5583a1810b6e7d5a
Tetyana <tsergata@gmail.com>
2020-02-24 17:10:05 +0100

    Inizio lavoro

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+// Array
+$db = [
+
+
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <title>Google FAQ</title>
+  </head>
+  <body>
+    <!-- Header -->
+    <header>
+      <div class="header-top clearfix">
+        <!-- <div class="box_logo clearfix"> -->
+          <a class="logo float" href="#">
+            <img src="img/download.png" alt="logo Google">
+          </a>
+          <h2 class="float">
+            <a href="#">Privacy e termini</a>
+          </h2>
+        <!-- </div> -->
+
+      </div>
+      <div class="header-bottom">
+      </div>
+    </header>
+    <!-- /Header -->
+    <!-- Main -->
+    <main>
+
+    </main>
+    <!-- /Main -->
+    <!-- Footer -->
+    <footer></footer>
+    <!-- /Footer -->
+
+
+  </body>
+</html>
